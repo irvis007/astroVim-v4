fc093d3577f129a65f1ae0fbbb8a4108f18d3198
lblazejowski <lblazejowski@pgs-soft.com>
2024-06-20 15:38:55 +0200

    correct misspelling

--- lua/community.lua
+++ lua/community.lua
@@ -1,5 +1,3 @@
-if true then return {} end -- WARN: REMOVE THIS LINE TO ACTIVATE THIS FILE
-
 -- AstroCommunity: import any community modules here
 -- We import this file in `lazy_setup.lua` before the `plugins/` folder.
 -- This guarantees that the specs are processed before any user plugins.
@@ -14,14 +12,15 @@ return {
   { import = "astrocommunity.pack.yaml" },
 
   -- utilities
-  { import = "astrocommunity.diagnostic.lsp_lines-nvim" },
+  { import = "astrocommunity.diagnostics.lsp_lines-nvim" },
   { import = "astrocommunity.editing-support.hypersonic-nvim" },
   { import = "astrocommunity.editing-support.multiple-cursors-nvim" },
   { import = "astrocommunity.editing-support.nvim-devdocs" },
-  { import = "astrocommunity.editing-support.rainbow-delimeters-nvim" },
+  { import = "astrocommunity.editing-support.rainbow-delimiters-nvim" },
   { import = "astrocommunity.editing-support.telescope-undo-nvim" },
   { import = "astrocommunity.editing-support.text-case-nvim" },
   { import = "astrocommunity.editing-support.todo-comments-nvim" },
-  { import = "astrocommunity.editing-support.auto-session-restore" },
-  { import = "astrocommunity.editing-support.neoscroll-nvim" },
+  { import = "astrocommunity.recipes.auto-session-restore" },
+  { import = "astrocommunity.scrolling.neoscroll-nvim" },
+  { import = "astrocommunity.file-explorer.telescope-file-browser-nvim" },
 }
